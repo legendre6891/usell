6a6eaac9874269df6376fcc5273b4913013311bd
sszuflita <sam.szuflita@gmail.com>
2013-04-20 20:01:35 -0700

    Added documentation to model.py

--- model.py
+++ model.py
@@ -1,7 +1,15 @@
+# File used to define the entity types within the data store.
+# Data is stored hierarchically: all items belong to a user,
+# all users belong to a network.
+
+
+# An object representing a network (a college/university/other
+# community group if we include non-colleges)
 class Network(db.Model):
     name = db.StringProperty()
     # some form of token
 
+# An object representing a user of our application
 class User(db.Model):
     first_name = db.StringProperty()
     last_name = db.StringProperty()
@@ -10,6 +18,7 @@ class User(db.Model):
     def fullName(self):
         return self.first_name + " " + self.last_name
 
+# An object representing an item within the marketplace
 class Item(db.Model):
     name = db.StringProperty()
     description = db.StringProperty(multiline=True)
